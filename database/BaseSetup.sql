2ff80174d9c8c61b585ddbbd5ab2879ecfba8bce
Valentin Secades <vsecades@qxdev.com>
2015-01-11 15:08:30 -0600

    Removed Schema creation info for import

--- database/BaseSetup.sql
+++ database/BaseSetup.sql
@@ -6,17 +6,6 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
-
 -- -----------------------------------------------------
 -- Table `mydb`.`languages`
 -- -----------------------------------------------------
